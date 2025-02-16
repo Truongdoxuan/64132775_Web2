d3aa00e552882419a87bee0288bbe7d16e62e94e
Xuân Trường <dxt8104@gmail.com>
2025-02-16 15:35:20 +0700

    completed 1.2

---
+++ Example_doPost/src/main/java/truongdx/vidu_doPost/Example.java
@@ -0,0 +1,62 @@
+package truongdx.vidu_doPost;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Example
+ */
+@WebServlet("/Example")
+public class Example extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Example() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter traVe =response.getWriter();
+		traVe.append("Bạn vừa gửi yêu cầu dạng GET, đây là đáp ứng của tôi");
+		String HTML = "<form method='POST' action='/Example_doPost/Example'>"
+	            + "<label> Họ: </label>"
+	            + "<input type='text' name='ho'><br>"
+	            + "<label> Tên: </label>"
+	            + "<input type='text' name='ten'><br>"
+	            + "<input type='submit' value='Gửi đi'>"
+	            + "</form>";
+		traVe.append(HTML);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		String value1=request.getParameter("ho");
+		String value2=request.getParameter("ten");
+		PrintWriter traVe =response.getWriter();
+		traVe.append("Bạn vừa gửi yêu cầu dạng POST, đây là đáp ứng của tôi");
+		traVe.append("\nHọ: ");
+		traVe.append(value1);
+		traVe.append("\nTên: ");
+		traVe.append(value2);
+	}
+
+}
