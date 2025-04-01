8c856db6b16549610edd5b9e56a51750e04a9d64
Xuân Trường <dxt8104@gmail.com>
2025-04-01 09:05:27 +0700

    push lần 1: tạo dự án

---
+++ DoXuanTruong_ThiGK/src/main/java/thiGK/ntu64132775/DoXuanTruong_ThiGK/DoXuanTruongThiGkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132775.DoXuanTruong_ThiGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoXuanTruongThiGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoXuanTruongThiGkApplication.class, args);
+	}
+
+}
---
+++ DoXuanTruong_ThiGK/src/test/java/thiGK/ntu64132775/DoXuanTruong_ThiGK/DoXuanTruongThiGkApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132775.DoXuanTruong_ThiGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoXuanTruongThiGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
