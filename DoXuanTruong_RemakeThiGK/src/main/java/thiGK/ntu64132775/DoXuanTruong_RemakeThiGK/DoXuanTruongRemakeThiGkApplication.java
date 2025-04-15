242eb75356cae2dd249462e7bc10aaa50cce7c4f
Xuân Trường <dxt8104@gmail.com>
2025-04-15 11:18:05 +0700

    push lần 1: tạo project

---
+++ DoXuanTruong_RemakeThiGK/src/main/java/thiGK/ntu64132775/DoXuanTruong_RemakeThiGK/DoXuanTruongRemakeThiGkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132775.DoXuanTruong_RemakeThiGK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoXuanTruongRemakeThiGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoXuanTruongRemakeThiGkApplication.class, args);
+	}
+
+}
---
+++ DoXuanTruong_RemakeThiGK/src/test/java/thiGK/ntu64132775/DoXuanTruong_RemakeThiGK/DoXuanTruongRemakeThiGkApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132775.DoXuanTruong_RemakeThiGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoXuanTruongRemakeThiGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
